5a2cad69c5c3ae1dfe097169c40c8c9e977d50f0
RodrigoPerezRP <rusbelrodrigoperez@gmail.com>
2026-01-17 13:30:38 -0500

    chang front to render 2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
 
-    API_URL_IMAGE: 'https://philosophical-florence-ecpar-8672eb7c.koyeb.app/',
-    API_URL: 'https://philosophical-florence-ecpar-8672eb7c.koyeb.app/api',
+    API_URL_IMAGE: 'https://ec-partituras-yape-back.onrender.com/',
+    API_URL: 'https://ec-partituras-yape-back.onrender.com/api',
 
 };
